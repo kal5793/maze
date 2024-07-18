8f9024d340b9d1038143a5f713e725ee49854f78
Hikaru <zeal23310178@fsgcl.jp>
2024-07-19 07:18:39 +0900

    迷路完成

--- mazeJava/src/CreateMaze.java
+++ mazeJava/src/CreateMaze.java
@@ -1,12 +1,16 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Random;
 import java.util.Stack;
 
-public class CreateMaze extends MyFrame{
-	private int[][] Maze;
-    public int Width =15;
-    public int Height=15;
+public class CreateMaze extends MyFrame implements KeyListener {
+    private int[][] Maze;
+    public int Width = 15;
+    public int Height = 15;
 
     // 乱数生成用
     private Random Random;
@@ -18,9 +22,16 @@ public class CreateMaze extends MyFrame{
     // 通路・壁情報
     private static final int Path = 0;
     private static final int Wall = 1;
+    private static final int Start = 2;
+    private static final int Goal = 3;
 
     // コンストラクタ
+    public void Maze() {
+        addKeyListener(this);
+    }
+
     public CreateMaze(int width, int height) {
+        addKeyListener(this);
         // 5未満のサイズや偶数では生成できない
         if (width < 15 || height < 15) ;
         if (width % 2 == 0) width++;
@@ -33,9 +44,13 @@ public class CreateMaze extends MyFrame{
         StartCells = new ArrayList<>();
         CurrentWallCells = new Stack<>();
         this.Random = new Random();
+        initializeMaze();
+        extendWall(1, 1);
+        setRandomStartAndGoal();
+        Draw();
     }
 
-    public int[][] createMaze() {
+    public int[][] initializeMaze() {
         // 各マスの初期設定を行う
         for (int y = 0; y < this.Height; y++) {
             for (int x = 0; x < this.Width; x++) {
@@ -122,8 +137,10 @@ public class CreateMaze extends MyFrame{
             }
         } else {
             // すべて現在拡張中の壁にぶつかる場合、バックして再開
-            Cell beforeCell = CurrentWallCells.pop();
-            extendWall(beforeCell.x, beforeCell.y);
+            if (!CurrentWallCells.isEmpty()) {
+                Cell beforeCell = CurrentWallCells.pop();
+                extendWall(beforeCell.x, beforeCell.y);
+            }
         }
     }
 
@@ -140,21 +157,120 @@ public class CreateMaze extends MyFrame{
         return CurrentWallCells.contains(new Cell(x, y));
     }
 
+    // スタート地点とゴール地点をランダムに設定し、必ず通路を確保
+    private void setRandomStartAndGoal() {
+        List<Cell> pathCells = new ArrayList<>();
+        for (int y = 1; y < Height - 1; y++) {
+            for (int x = 1; x < Width - 1; x++) {
+                if (Maze[x][y] == Path) {
+                    pathCells.add(new Cell(x, y));
+                }
+            }
+        }
+
+        // ランダムにスタート地点とゴール地点を選択
+        Cell startCell = pathCells.remove(Random.nextInt(pathCells.size()));
+        Cell goalCell = pathCells.remove(Random.nextInt(pathCells.size()));
+
+        // BFSでスタート地点からゴール地点までの経路を確保
+        ensurePath(startCell, goalCell);
+
+        Maze[startCell.x][startCell.y] = Start;
+        Maze[goalCell.x][goalCell.y] = Goal;
+    }
+
+    // スタート地点からゴール地点までの経路を確保
+    private void ensurePath(Cell start, Cell goal) {
+        Queue<Cell> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[Width][Height];
+        queue.add(start);
+        visited[start.x][start.y] = true;
+
+        while (!queue.isEmpty()) {
+            Cell current = queue.poll();
+            if (current.equals(goal)) {
+                break;
+            }
+
+            for (Direction direction : Direction.values()) {
+                int newX = current.x;
+                int newY = current.y;
+
+                switch (direction) {
+                    case Up:
+                        newY -= 1;
+                        break;
+                    case Right:
+                        newX += 1;
+                        break;
+                    case Down:
+                        newY += 1;
+                        break;
+                    case Left:
+                        newX -= 1;
+                        break;
+                }
+
+                if (newX > 0 && newX < Width - 1 && newY > 0 && newY < Height - 1 && !visited[newX][newY] && Maze[newX][newY] == Path) {
+                    queue.add(new Cell(newX, newY));
+                    visited[newX][newY] = true;
+                }
+            }
+        }
+
+        // 経路を作成（BFSの訪問済みのノードを元に）
+        for (int y = 1; y < Height - 1; y++) {
+            for (int x = 1; x < Width - 1; x++) {
+                if (visited[x][y]) {
+                    Maze[x][y] = Path;
+                }
+            }
+        }
+    }
+
     // デバッグ用処理
     public static void debugPrint(int[][] maze) {
         System.out.println("Width: " + maze.length);
         System.out.println("Height: " + maze[0].length);
         for (int y = 0; y < maze[0].length; y++) {
             for (int x = 0; x < maze.length; x++) {
-                System.out.print(maze[x][y] == Wall ? "■" : "　");
+                switch (maze[x][y]) {
+                    case Wall:
+                        System.out.print("■");
+                        break;
+                    case Start:
+                        System.out.print("S");
+                        break;
+                    case Goal:
+                        System.out.print("G");
+                        break;
+                    default:
+                        System.out.print("　");
+                        break;
+                }
             }
             System.out.println();
         }
     }
 
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            new CreateMaze(17, 17);
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+
     // セル情報
     private static class Cell {
         int x, y;
+
         Cell(int x, int y) {
             this.x = x;
             this.y = y;
@@ -178,18 +294,30 @@ public class CreateMaze extends MyFrame{
     private enum Direction {
         Up, Right, Down, Left
     }
-    
+
     private void Draw() {
         int drawXpos = 10;
         int drawYpos = 40;
-        int startY=40;
-        int wallSize =30;
+        int startY = 40;
+        int wallSize = 5;
 
         for (int i = 0; i < Width; i++) {
             for (int j = 0; j < Height; j++) {
-
-                fillRect(drawXpos, drawYpos,30 ,30 );
-                setColor(0,0,0);
+                switch (Maze[i][j]) {
+                    case Path://道
+                        setColor(255, 255, 255);
+                        break;
+                    case Wall:
+                        setColor(0, 255, 0);
+                        break;
+                    case Start:
+                        setColor(0, 0, 255); // スタート地点は青色
+                        break;
+                    case Goal:
+                        setColor(255, 0, 0); // ゴール地点は赤色
+                        break;
+                }
+                fillRect(drawXpos, drawYpos, wallSize, wallSize);
                 drawYpos += wallSize;
             }
             drawXpos += wallSize;
